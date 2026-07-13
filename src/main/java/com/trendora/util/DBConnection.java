75cea25c2e28ec6f7bdce1941087f207db09dff8
srushti-1123 <srushtipatil1123.in>
2026-07-13 15:14:52 +0530

    Added Dockerfile for Render Deployment

--- src/main/java/com/trendora/util/DBConnection.java
+++ src/main/java/com/trendora/util/DBConnection.java
@@ -6,9 +6,15 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/trendora?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "password";
+    private static final String URL = System.getenv("DB_URL") != null ?
+            System.getenv("DB_URL") :
+            "jdbc:mysql://localhost:3306/trendora?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+
+    private static final String USERNAME = System.getenv("DB_USERNAME") != null ?
+            System.getenv("DB_USERNAME") : "root";
+
+    private static final String PASSWORD = System.getenv("DB_PASSWORD") != null ?
+            System.getenv("DB_PASSWORD") : "Srushtibharath1123";
 
     public static Connection getConnection() {
         Connection connection = null;
